c35d710368ba47e16ab73aa24adfec0dadc3d56b
SATO Kentaro <kentaro@ranvis.com>
2022-03-24 23:41:07 +0900

    PHP 8.0 compatibility.

--- src/compatibility.h
+++ src/compatibility.h
@@ -5,4 +5,13 @@
 
 #if PHP_API_VERSION < 20210902 /* <PHP8.1 */
 extern bool zend_array_is_list(zend_array *array);
+
+static zend_long php81_zend_atol(const char *s)
+{
+	zend_long i;
+	ZEND_ATOL(i, s);
+	return i;
+}
+#undef ZEND_ATOL
+#define ZEND_ATOL(s)  php81_zend_atol(s)
 #endif
