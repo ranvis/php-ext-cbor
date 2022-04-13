eada404fb477fa808e32a9c9e8702e92a79f8dbd
SATO Kentaro <kentaro@ranvis.com>
2022-04-13 19:30:32 +0900

    Fix prototype.
    Not the default array, but the default keys.

--- src/cbor.stub.php
+++ src/cbor.stub.php
@@ -5,14 +5,6 @@
  * @generate-function-entries
  */
 
-function cbor_encode(mixed $value, int $flags = CBOR_BYTE | CBOR_KEY_BYTE, ?array $options = [
-    'max_depth' => 64,
-    'string_ref' => false,
-]): string {}
+function cbor_encode(mixed $value, int $flags = CBOR_BYTE | CBOR_KEY_BYTE, ?array $options = null): string {}
 
-function cbor_decode(string $data, int $flags = CBOR_BYTE | CBOR_KEY_BYTE, ?array $options = [
-    'max_depth' => 64,
-    'max_size' => 65536,
-    'string_ref' => true,
-    'shared_ref' => true,
-]): mixed {}
+function cbor_decode(string $data, int $flags = CBOR_BYTE | CBOR_KEY_BYTE, ?array $options = null): mixed {}
--- src/cbor_arginfo.h
+++ src/cbor_arginfo.h
@@ -1,16 +1,16 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 731e37372f72a62a02f67691eb68875decbb0aaa */
+ * Stub hash: 9fffe711824d08a1f3a6c710b2b2cacbba629de6 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_cbor_encode, 0, 1, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, value, IS_MIXED, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flags, IS_LONG, 0, "CBOR_BYTE | CBOR_KEY_BYTE")
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, options, IS_ARRAY, 1, "[\'max_depth\' => 64, \'string_ref\' => false]")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, options, IS_ARRAY, 1, "null")
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_cbor_decode, 0, 1, IS_MIXED, 0)
 	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flags, IS_LONG, 0, "CBOR_BYTE | CBOR_KEY_BYTE")
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, options, IS_ARRAY, 1, "[\'max_depth\' => 64, \'max_size\' => 65536, \'string_ref\' => true, \'shared_ref\' => true]")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, options, IS_ARRAY, 1, "null")
 ZEND_END_ARG_INFO()
 
 
